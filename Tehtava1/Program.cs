e37be81fa8358a02f0650fb8658de56ec82e081d
jerel <jerel@pytis>
2017-02-02 00:37:02 +0200

    t1 ready-to-go

--- Tehtava1/Program.cs
+++ Tehtava1/Program.cs
@@ -10,6 +10,23 @@
  * How many times you want to throw a dice :  10000
 
  * Dice is now thrown 10000 times, average is 3,4853
+ * 
+ * +tehtävä. Tulosta lopuksi kaikkien heitettyjen lukujen esiintymismäärät.
+
+
+    How many times you want to throw a dice :  10000
+
+    Dice is now thrown 10000 times
+    - average is 3,4853
+    - 1 count is 1726
+    - 2 count is 1610
+    - 3 count is 1705
+    - 4 count is 1691
+    - 5 count is 1580
+    - 6 count is 1688
+
+    Press enter key to continue...
+    
  *
  * Jere Liikka 1.2.2017
  */
@@ -24,8 +41,8 @@ using System.Diagnostics;
 namespace JAMK.IT
 {
     class Program
-    { 
-        static void Main(string[] args)
+    {
+        static void Main()
         {
             var sw = new Stopwatch();
             List<float> Numerolista = new List<float>();
@@ -53,16 +70,27 @@ namespace JAMK.IT
                 LaskeNumerot = LaskeNumerot + Numerolista[i];
             }
             double KeskiArvo = LaskeNumerot / Numerolista.Count;
-            Console.WriteLine("Noppaa heitetty {0} kertaa, keskiarvona {1}",OnNumero, KeskiArvo);
+            Console.WriteLine("Noppaa heitetty {0} kertaa, keskiarvona {1}", OnNumero, KeskiArvo);
             sw.Stop();
             var MennytAika = sw.ElapsedMilliseconds;
-            if (MennytAika > 2000)
+            Console.WriteLine("Numeroiden laskemiseen meni {0} millisekuntia!", MennytAika);
+            Numerolista.Sort();
+            //
+            var dictionary = new Dictionary<int, int>();
+            int numero = 0;
+            for (int i = 0; i < Numerolista.Count; i++)
             {
-                Console.WriteLine("Noin monen numeron laskemiseen meni {0} millisekuntia!", MennytAika);
+                numero = Convert.ToInt32(Math.Ceiling(Numerolista[i]));
+                dictionary.Add(i, numero);
             }
-
-
-
+            Dictionary<int, int> EritteleNumerot = new Dictionary<int, int>();
+            foreach (int i in dictionary.Values)
+                if (EritteleNumerot.ContainsKey(i))
+                    EritteleNumerot[i]++;
+                else
+                    EritteleNumerot[i] = 1;
+            foreach (KeyValuePair<int, int> kvp in EritteleNumerot)
+                Console.WriteLine("Numeroa {0} heitetty {1}", kvp.Key, kvp.Value);
         }
     }
 }
