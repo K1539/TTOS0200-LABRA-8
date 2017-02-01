0c67ced85f2901ff9a2e63603991c0c86580ba47
jerel <jerel@pytis>
2017-02-01 19:08:40 +0200

    t1

--- Tehtava1/Program.cs
+++ Tehtava1/Program.cs
@@ -19,6 +19,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using JAMK.IT;
+using System.Diagnostics;
 
 namespace JAMK.IT
 {
@@ -26,10 +27,39 @@ namespace JAMK.IT
     { 
         static void Main(string[] args)
         {
+            var sw = new Stopwatch();
+            List<float> Numerolista = new List<float>();
             Noppa noppa = new Noppa();
-            Console.WriteLine(noppa.NoppaLuku);
-            Console.WriteLine(noppa.NoppaLuku);
-            Console.WriteLine(noppa.NoppaLuku);
+            Console.WriteLine("Montako kertaa heitetään noppaa? > ");
+            string Valinta = Console.ReadLine();
+            double OnNumero;
+            double LaskeNumerot = 0;
+            bool OnkoNumero;
+            while (true)
+            {
+                if (OnkoNumero = double.TryParse(Valinta, out OnNumero))
+                {
+                    break;
+                }
+                else { Console.WriteLine("Et valinnut numeroa, yritä uudestaan > "); }
+            }
+            sw.Start();
+            for (int i = 0; i < OnNumero; i++)
+            {
+                Numerolista.Add(noppa.NoppaLuku);
+            }
+            for (int i = 0; i < Numerolista.Count; i++)
+            {
+                LaskeNumerot = LaskeNumerot + Numerolista[i];
+            }
+            double KeskiArvo = LaskeNumerot / Numerolista.Count;
+            Console.WriteLine("Noppaa heitetty {0} kertaa, keskiarvona {1}",OnNumero, KeskiArvo);
+            sw.Stop();
+            var MennytAika = sw.ElapsedMilliseconds;
+            if (MennytAika > 2000)
+            {
+                Console.WriteLine("Noin monen numeron laskemiseen meni {0} millisekuntia!", MennytAika);
+            }
 
 
 
