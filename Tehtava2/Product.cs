0e3aa979b2b5d656fdcf6634f176f4a6d00ef750
jerel <jerel@pytis>
2017-02-02 22:53:16 +0200

    t2

---
+++ Tehtava2/Product.cs
@@ -0,0 +1,30 @@
+﻿/*
+ *  Ostettavalta tuotteelta riittää käsitellä nimi ja hinta. Toteuta Product-luokka
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT
+{
+    public class Product
+    {
+        public string Nimi { get; set; }
+        public double Hinta { get; set; }
+
+        public Product()
+        { }
+
+        public Product(string nimi, double hinta)
+        {
+            Nimi = nimi;
+            Hinta = hinta;
+        }
+        public override string ToString()
+        {
+            return "-Tuote : " + Nimi + " " + Hinta + " e";
+        }
+    }
+}
