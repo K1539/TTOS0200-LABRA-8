4d872abd0f400b476e7c69e2e3c7c4de21539d91
K1539 <K1539@ICT-421-WS-37>
2017-02-06 10:43:11 +0200

    t6test

---
+++ Tehtava5Tests/ArrayCalcsTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tehtava5;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtava5.Tests
+{
+    [TestClass()]
+    public class ArrayCalcsTests
+    {
+        double[] array = { 1.0, 2.0, 3.3, 5.5, 6.3, -4.5, 12.0 };
+
+        [TestMethod()]
+        public void SumTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AverageTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MinTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MaxTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ Tehtava6Tests/LaskuTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JAMK.IT;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT.Tests
+{
+    [TestClass()]
+    public class LaskuTests
+    {
+
+        [TestMethod()]
+        public void BillTest()
+        {
+            Lasku lasku = new Lasku();
+            float expected = (0.75f * 3) + (0.90f * 4) + 3.95f + 1.20f;
+
+            lasku.Tuotteet.Add(new Product("Makkara", 0.75f), 3);
+            lasku.Tuotteet.Add(new Product("Kalja", 0.90f), 4);
+            lasku.Tuotteet.Add(new Product("Kahvi", 3.95f), 1);
+            lasku.Tuotteet.Add(new Product("Sipsi", 1.20f), 1);
+            lasku.Bill();
+
+            float actual = lasku.Kokonaishinta;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
